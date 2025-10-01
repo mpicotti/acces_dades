aaeeeac53defc2ae38b9d7d98602d99090e0ff3d
Marti Picotti <mpicotti472@boscdelacoma.cat>
2025-10-01 20:11:15 +0200

    Codi Miquel + Creacio Base de Dades

---
+++ Exercici1/Model/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Botiga.Model;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Code { get; set; } = "";
+    public string Name { get; set; } = "";
+    public decimal Price { get; set; }
+}
